ba350b693ae8587cdcaf837e70a0062340f51191
Shari Bradford <shari.bradford@gmail.com>
2020-07-05 14:51:26 -0400

    added classes Author and Book to models

--- book_authors_app/models.py
+++ book_authors_app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    # id INT
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+
+    def __repr__(self):
+        return f"<Author object: {self.first_name} {self.last_name} ({self.id})>"
+
+class Book(models.Model):
+    # id INT
+    title = models.CharField(max_length=255)
+    desc = models.TextField()
+    author = models.ManyToManyField(Author,related_name="books")
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+
+    def __repr__(self):
+        return f"<Book object: {self.title} ({self.id})>"
